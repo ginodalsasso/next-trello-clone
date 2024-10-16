bb0be82b70cb78830808dd10b12b9302bb720a38
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-10-16 17:34:23 +0200

    initialising clerk auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,17 @@
+import { ClerkProvider } from '@clerk/nextjs'
+
+
+const PlatformLayout = ({ 
+    children 
+
+}: {
+    children: React.ReactNode // ReactNode est un type de données qui accepte n'importe quel type de données
+}) => {
+    return (
+        <ClerkProvider>
+            { children }
+        </ClerkProvider>
+    )
+}
+
+export default PlatformLayout;
