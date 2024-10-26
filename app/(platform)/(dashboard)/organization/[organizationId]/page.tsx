abbd0fa34d0e19f1cac69cb02f552252bcc93348
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-10-26 15:55:47 +0200

    creating test with zod first data

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,14 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
-    async function create (formData: FormData) {
-        "use server";
 
-        const title = formData.get("title") as string;
+const OrganizationIdPage = async () => {
 
-        await db.board.create({
-            data: {
-                title,
-            }
-        })
-    }
+    const boards = await db.board.findMany();
 
     return (
-        <div>
+        <div className="flex flex-col space-y-4">
             <form action={create}>
                 <input 
                     id="title"
@@ -23,7 +17,17 @@ const OrganizationIdPage = () => {
                     placeholder="Enter a board title"
                     className="border-black border p-1"
                 />
+                <Button type="submit">
+                    Submit
+                </Button>
             </form>
+            <div className="space-y-2">
+                { boards.map((board) => (
+                    <div key= { board.id }>
+                        { board.title }
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
